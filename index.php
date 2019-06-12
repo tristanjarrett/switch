579d453cab1392dad3b2fe6bc0a6cf99cc910670
Tristan Jarrett <tristan.jarrett@outlook.com>
2019-06-12 17:11:21 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+  <head>
+    <title>Switch</title>
+    <link href="style.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <header class="header">
+      <div class="container-md flex">
+        <span class="brand">Switch</span>
+        <a href="#" class="button">Download</a>
+      </div>
+    </header>
+
+    <div class="jumbotron">
+      <div class="container-md">
+        <h1>Jumbotron</h1>
+      </div>
+    </div>
+
+    <div class="container-sm">
+      <div class="popout">
+      
+      </div>
+    </div>
+
+    <div class="main">
+      <div class="container-md">
+        
+      </div>
+    </div>
+
+    <footer class="footer">
+      <div class="container-md">
+        <span>&copy; <?php echo date('Y'); ?> Switch</span>
+      </div>
+    </footer>
+
+  </body>
+</html>
